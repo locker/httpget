7e1defb3525cf22d4763adf3b8744b93d0dbe86c
Vladimir Davydov <vdavydov@me.com>
2016-07-10 00:02:43 +0300

    Print download progress
    And add option -q to suppress it.
    
    Signed-off-by: Vladimir Davydov <vdavydov@me.com>

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <time.h>
 #include <errno.h>
 #include <assert.h>
 
@@ -28,6 +29,7 @@
 static char *PROG_NAME;
 static char *URL;
 static char *OUTPUT_FILE;
+static bool QUIET;
 
 static int output_fd = -1;
 static struct url_struct url;
@@ -52,6 +54,7 @@ static void print_help(void)
 	       "Options:\n"
 	       "  -o FILE       write document to FILE\n"
 	       "                (use `-' for stdandard output)\n"
+	       "  -q            quiet (no output)\n"
 	       "  -v            increase output verbosity\n"
 	       "                (useful for debugging)\n"
 	       "  -h            print this help and exit\n",
@@ -82,10 +85,13 @@ static void parse_args(int argc, char *argv[])
 
 	PROG_NAME = argv[0];
 
-	while ((c = getopt(argc, argv, "o:vh")) != -1) {
+	while ((c = getopt(argc, argv, "o:qvh")) != -1) {
 		switch (c) {
 		case 'o':
 			OUTPUT_FILE = optarg;
+			break;
+		case 'q':
+			QUIET = true;
 			break;
 		case 'v':
 			http_dump_fn = printf_stderr;
@@ -168,6 +174,64 @@ static void output(const char *buf, size_t size)
 	}
 }
 
+/* Print @c @n times */
+static void fputcn(int c, int n, FILE *stream)
+{
+	while (n-- > 0)
+		fputc(c, stream);
+}
+
+static void print_progress(size_t read, size_t total, bool done)
+{
+	static time_t begin, last_update;
+	static int line_len;
+
+	time_t now;
+	size_t rate;
+
+	if (QUIET)
+		return;
+
+	now = time(NULL);
+
+	/* Do not redraw more often than once a second */
+	if (!done && now - last_update == 0)
+		return;
+
+	last_update = now;
+
+	/* Convert to KB */
+	read >>= 10;
+	total >>= 10;
+
+	/* Remove the previous progress line */
+	fputc('\r', stderr);
+	fputcn(' ', line_len, stderr);
+	fputc('\r', stderr);
+
+	if (!begin)
+		begin = now;
+
+	rate = read / (now - begin + 1);
+
+	/*
+	 * We can't terminate the line with '\n', because this way we couldn't
+	 * easily redraw it by sending '\r'.
+	 */
+	line_len = fprintf(stderr,
+			   "Downloaded %zu/%zu kB in %d second(s), "
+			   "average rate: %zu kB/s",
+			   read, total, (int)(now - begin), rate);
+	if (total >= read)
+		line_len += fprintf(stderr, ", time left: %d s",
+				    (int)((total - read) / (rate + 1)));
+
+	if (done)
+		fputc('\n', stderr);
+	else
+		fflush(stderr);
+}
+
 static void download_http(void)
 {
 	struct http_request_info info = {
@@ -191,6 +255,7 @@ static void download_http(void)
 	open_output_file();
 	while (1) {
 		n = http_response_read(&resp, buf, BUF_SIZE);
+		print_progress(resp.body_read, resp.body_size, n <= 0);
 		if (n < 0)
 			fail("%s", http_last_error());
 		if (!n)
